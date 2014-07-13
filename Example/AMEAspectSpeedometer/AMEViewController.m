d28a178ed450616bf13c54aeb61bde89d62c5fdb
ainame <s.namai.09@gmail.com>
2014-07-13 20:54:10 +0900

    update demo

--- Example/AMEAspectSpeedometer/AMEViewController.m
+++ Example/AMEAspectSpeedometer/AMEViewController.m
@@ -7,13 +7,14 @@
 //
 
 #import "AMEViewController.h"
-#import "AMESpeedometer.h"
+#import <AMEAspectSpeedometer.h>
 
 @interface AMEViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *start;
 @property (weak, nonatomic) IBOutlet UIButton *finish;
 @property (weak, nonatomic) IBOutlet UILabel *label;
 @property (nonatomic, strong) AMESpeedometer *speedometer;
+@property (nonatomic, strong) AMEAspectSpeedometer *aspectSpeedometer;
 @end
 
 @implementation AMEViewController
@@ -22,7 +23,24 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    _speedometer = [[AMESpeedometer alloc] initWithReportName:@"stop watch"];
+    _aspectSpeedometer = [AMEAspectSpeedometer new];
+    [_aspectSpeedometer measureWithName:@"test return primitive value"
+                                 target:self
+                                 around:@selector(returnInteger)
+                       usingReportBlock:^(AMESpeedometerReport *report) { NSLog(@"%@", report); }];
+    [_aspectSpeedometer measureWithName:@"test return object value"
+                                 target:self
+                                 around:@selector(returnNumber)
+                       usingReportBlock:^(AMESpeedometerReport *report) { NSLog(@"%@", report); }];
+    [_aspectSpeedometer measureWithName:@"test tap start and finish"
+                                 target:self
+                                  start:@selector(tapStartButton:)
+                                 finish:@selector(tapFinishButton:)
+                       usingReportBlock:^(AMESpeedometerReport *report) {
+                           NSLog(@"%@", report);
+                           self.label.text = [NSString stringWithFormat:@"elapsed time: %f", report.elapsedTime];
+                       }];
+    NSLog(@"%ld, %@", (long)[self returnInteger], [self returnNumber]);
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,16 +49,24 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)returnInteger
+{
+    return 1;
+}
+
+- (NSNumber *)returnNumber
+{
+    return @1;
+}
+
 - (IBAction)tapStartButton:(UIButton *)sender
 {
-    [_speedometer start];
+    self.label.text = @"it started.";
 }
 
 - (IBAction)tapFinishButton:(id)sender
 {
-    [_speedometer finish];
-    AMESpeedometerReport *report = [_speedometer report];
-    self.label.text = [NSString stringWithFormat:@"elapsed time: %f", report.elapsedTime];
+    NSLog(@"tapped!");
 }
 
 @end
